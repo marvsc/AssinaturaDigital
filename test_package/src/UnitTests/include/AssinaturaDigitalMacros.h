f2d4d6e9bbcd71c63fe8767bd42f6e431fdd8942
Marcus Vinicius dos Santos Chaves <marcus.chaves@digitro.com>
2026-02-24 17:50:56 -0300

    Correção

--- test_package/src/UnitTests/include/AssinaturaDigitalMacros.h
+++ test_package/src/UnitTests/include/AssinaturaDigitalMacros.h
@@ -11,7 +11,7 @@
 #define PKCS12_FILE_PATH "certificado_teste_hub.pfx"
 #define AES_KEY "b90XmFVR51L485rxXXCRhupVxva0yDFh"
 #define AES_INITIALIZATION_VECTOR "NSVmgGXSm2jRTiyq"
-#define PKCS12_ENVVAR_PASSWORD "PKSC12_ENVVAR_PASSWORD"
+#define PKCS12_ENVVAR_PASSWORD "PKCS12_ENVVAR_PASSWORD"
 #define FILE_TO_ASSIGN_PATH "doc.txt"
 #define SIGNATURE_FILE_PATH "../../resources/arquivos/signature.p7s"
 
