aea415f39242ea38c9c9bd3b7ac907fa4a983b71
marcus.chaves <marcus.chaves@D1385.digitro.corp>
2026-04-07 12:26:31 -0300

    Evolução

--- test_package/src/UnitTests/include/OpenSSLUtilsTest.h
+++ test_package/src/UnitTests/include/OpenSSLUtilsTest.h
@@ -1,8 +1,8 @@
 /*
- * OpenSSLUtilsTest.h
- *
- *  Created on: 2 de abr. de 2026
- *      Author: marcus.chaves
+ * @file OpenSSLUtilsTest.h
+ * @brief Declaração da classe OpenSSLUtilsTest
+ * @author Marcus Chaves
+ * @date 2026-01-27
  */
 
 #ifndef TEST_PACKAGE_SRC_UNITTESTS_INCLUDE_OPENSSLUTILSTEST_H_
@@ -12,13 +12,29 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 
+/*
+ * @class CMSSignerTest
+ * @brief Classe de testes unitários para o OpenSSLUtils
+ */
 class OpenSSLUtilsTest: public CppUnit::TestFixture {
+    // Declaração do suite de testes
     CPPUNIT_TEST_SUITE(OpenSSLUtilsTest);
+    // Adicionando steps
     CPPUNIT_TEST(teste_decrypt_aes_256_cbc);
     CPPUNIT_TEST(teste_obter_ca_chain_a_partir_do_certificado);
     CPPUNIT_TEST_SUITE_END();
+
 public:
+
+    /*
+     * @brief Teste para decriptar string usando algoritmo AES-256-CBC.
+     */
     void teste_decrypt_aes_256_cbc();
+
+    /*
+     * @brief Teste para obter a cadeia de certificados da autoridade certificadora
+     *          a partir do certificado gerado.
+     */
     void teste_obter_ca_chain_a_partir_do_certificado();
 };
 
--- test_package/src/UnitTests/src/OpenSSLUtilsTest.cpp
+++ test_package/src/UnitTests/src/OpenSSLUtilsTest.cpp
@@ -1,9 +1,3 @@
-/*
- * OpenSSLUtilsTest.cpp
- *
- *  Created on: 2 de abr. de 2026
- *      Author: marcus.chaves
- */
 
 #include "../include/OpenSSLUtilsTest.h"
 
@@ -14,18 +8,24 @@
 #include <Poco/Crypto/PKCS12Container.h>
 #include <Poco/Crypto/X509Certificate.h>
 
+// Registra o suite de testes
 CPPUNIT_TEST_SUITE_REGISTRATION(OpenSSLUtilsTest);
 
 void OpenSSLUtilsTest::teste_decrypt_aes_256_cbc() {
+    // Decripta a senha obtida em uma variável de ambiente
     CPPUNIT_ASSERT_MESSAGE(CPPUNIT_PRINTF_MESSAGE("Não foi possível decriptar o texto %s", std::getenv(PKCS12_ENVVAR_PASSWORD)),
             OpenSSLUtils::decrypt_aes_256_cbc(std::getenv(PKCS12_ENVVAR_PASSWORD), AES_KEY,
                     reinterpret_cast<const unsigned char*>(AES_INITIALIZATION_VECTOR)).size() > 0);
 }
 
 void OpenSSLUtilsTest::teste_obter_ca_chain_a_partir_do_certificado() {
+    // Instancia o container PKCS 12
     Poco::Crypto::PKCS12Container container(PKCS12_FILE_PATH,
             OpenSSLUtils::decrypt_aes_256_cbc(std::getenv(PKCS12_ENVVAR_PASSWORD), AES_KEY,
                     reinterpret_cast<const unsigned char*>(AES_INITIALIZATION_VECTOR)));
+
+    // Obtém a cadeia de certificados da autoridade certificadora a partir do certificado obtido no arquivo
+    // PKCS 12.
     CPPUNIT_ASSERT_MESSAGE(CPPUNIT_PRINTF_MESSAGE("Não foi possível obter a cadeia de certificados da "
             "autoridade certificadora emissora do certificado %s", PKCS12_FILE_PATH),
             OpenSSLUtils::get_ca_cert_chain(container.getX509Certificate().certificate()).size() > 0);
